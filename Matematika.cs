f771df00b0a6107337a5927e3431b4dfba7cc6a1
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2021-11-21 02:00:27 +0700

    Praktikum 6

--- Matematika.cs
+++ Matematika.cs
@@ -12,29 +12,90 @@ namespace ServiceMtk_P1_20190140089
     {
         public int Bagi(int a, int b)
         {
+            try
+            {
+                return a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
 
-            return a / b;
+                MathFault mf = new MathFault();
+                mf.Kode = "Input Yang Anda Masukkan Salah";
+                mf.Pesan = "Masukkan Input Yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
+
+
         }
         public int Kali(int a, int b)
         {
-            return a * b;
+            try
+            {
+                return a * b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input Yang Anda Masukkan Salah";
+                mf.Pesan = "Masukkan Input Yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
         public int Kurang(int a, int b)
         {
-            return a - b;
+            try
+            {
+                return a - b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input Yang Anda Masukkan Salah";
+                mf.Pesan = "Masukkan Input Yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
         public int Tambah(int a, int b)
         {
-            return a + b;
+            try
+            {
+                return a + b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input Yang Anda Masukkan Salah";
+                mf.Pesan = "Masukkan Input Yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
         //TKoordinat:nama methodnya, a,b = input seperti int.
         //Membuat koordinat hasil lalu menjumlahkan xa dengan xb, ya dengan yb lalu dimasukkan ke hasil
         public Koordinat TKoordinat(Koordinat a, Koordinat b)
         {
-            Koordinat hasil = new Koordinat();
-            hasil.X = a.X + b.X;
-            hasil.Y = a.Y + b.Y;
-            return hasil;
+            try
+            {
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input Yang Anda Masukkan Salah";
+                mf.Pesan = "Masukkan Input Yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
         }
     }
 }
